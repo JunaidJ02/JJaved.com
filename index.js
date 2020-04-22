0db3b32175ef65d3a0227f379f824eba0b10af86
junaidjaved <35304526+JunaidJ02@users.noreply.github.com>
2020-04-22 06:44:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 455 || document.documentElement.scrollTop > 455) {
+  if (document.body.scrollTop > (6*window.innerHeight)/13 || document.documentElement.scrollTop > (6*window.innerHeight)/13) {
     document.getElementById("headerName").style.visibility = "visible";
     document.getElementById("centerName").style.visibility = "hidden";
   } else {
