cf36ccce07e610704e716e953328517ef324e2b5
junaidjaved <35304526+JunaidJ02@users.noreply.github.com>
2020-04-22 04:03:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 455 || document.documentElement.scrollTop > 50) {
-    document.getElementById("name").style.fontSize = "1.2rem";
-    document.getElementById("name").style.visibility = "visible";
-    document.getElementById("name1").style.visibility = "hidden";
+    document.getElementById("headerName").style.visibility = "visible";
+    document.getElementById("centerName").style.visibility = "hidden";
   } else {
-    document.getElementById("name").style.fontSize = "1.8rem";
-    document.getElementById("name").style.visibility = "hidden";
-    document.getElementById("name1").style.visibility = "visible";
+    document.getElementById("headerName").style.visibility = "hidden";
+    document.getElementById("centerName").style.visibility = "visible";
   }
 }
