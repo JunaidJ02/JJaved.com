51550f54a508baa1821f92e3b2eea4115eeda990
junaidjaved <35304526+JunaidJ02@users.noreply.github.com>
2020-04-22 09:00:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > (19*window.innerHeight)/40 || document.documentElement.scrollTop > (6*window.innerHeight)/13) {
+  if (document.body.scrollTop > (75*window.innerHeight)/160 || document.documentElement.scrollTop > (6*window.innerHeight)/13) {
     document.getElementById("headerName").style.visibility = "visible";
-//     document.getElementById("about").style.visibility = "visible";
-//     document.getElementById("contact").style.visibility = "visible";
     document.getElementById("centerName").style.visibility = "hidden";
+    // document.getElementById("about").style.visibility = "visible";
+    // document.getElementById("contact").style.visibility = "visible";
   } else {
     document.getElementById("headerName").style.visibility = "hidden";
-//     document.getElementById("about").style.visibility = "hidden";
-//     document.getElementById("contact").style.visibility = "hidden";
     document.getElementById("centerName").style.visibility = "visible";
+    // document.getElementById("about").style.visibility = "hidden";
+    // document.getElementById("contact").style.visibility = "hidden";
   }
 }
 
 function loaded(){
   if (window.innerWidth < 1000) {
     document.getElementById("codeBlocks").style.visibility = "hidden";
-//     document.getElementById("about").style.visibility = "hidden";
-//     document.getElementById("contact").style.visibility = "hidden";
+    // document.getElementById("about").style.visibility = "hidden";
+    // document.getElementById("contact").style.visibility = "hidden";
   }
 }
