1fc5aa733e8bef121fb4312d98febe57dc264b6f
junaidjaved <35304526+JunaidJ02@users.noreply.github.com>
2020-04-22 08:52:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > (6*window.innerHeight)/13 || document.documentElement.scrollTop > (6*window.innerHeight)/13) {
+  if (document.body.scrollTop > (19*window.innerHeight)/40 || document.documentElement.scrollTop > (6*window.innerHeight)/13) {
     document.getElementById("headerName").style.visibility = "visible";
+    document.getElementById("about").style.visibility = "visible";
+    document.getElementById("contact").style.visibility = "visible";
     document.getElementById("centerName").style.visibility = "hidden";
   } else {
     document.getElementById("headerName").style.visibility = "hidden";
+    document.getElementById("about").style.visibility = "hidden";
+    document.getElementById("contact").style.visibility = "hidden";
     document.getElementById("centerName").style.visibility = "visible";
   }
 }
@@ -13,5 +17,7 @@ function scrollFunction() {
 function loaded(){
   if (window.innerWidth < 1000) {
     document.getElementById("codeBlocks").style.visibility = "hidden";
+    document.getElementById("about").style.visibility = "hidden";
+    document.getElementById("contact").style.visibility = "hidden";
   }
 }
