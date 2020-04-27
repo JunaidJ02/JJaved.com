aa3ed7a06fd3eaf25ee6e4a1eeb68bac4dad9650
junaidjaved <35304526+JunaidJ02@users.noreply.github.com>
2020-04-27 15:57:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-// function scrollFunction() {
-//   var centerName = document.getElementById("centerName");
-//   var centerNameBounding = centerName.getBoundingClientRect();
-//   if (centerNameBounding.top < 4) {
-//     document.getElementById("headerName").style.visibility = "visible";
-//     document.getElementById("centerName").style.visibility = "hidden";
-//   } else {
-//     document.getElementById("headerName").style.visibility = "hidden";
-//     document.getElementById("centerName").style.visibility = "visible";
-//   }
-// }
+function scrollFunction() {
+  var centerName = document.getElementById("centerName");
+  var centerNameBounding = centerName.getBoundingClientRect();
+  if (centerNameBounding.top < 4) {
+    document.getElementById("headerName").style.visibility = "visible";
+    document.getElementById("centerName").style.visibility = "hidden";
+  } else {
+    document.getElementById("headerName").style.visibility = "hidden";
+    document.getElementById("centerName").style.visibility = "visible";
+  }
+
+  if (document.getElementById("main").scrollTop > 200) {
+    console.log("transitioning");
+    document.getElementById("aboutMePicture").className = "slideUp";
+  }
+}
 
 function loaded(){
   console.log("loaded");
